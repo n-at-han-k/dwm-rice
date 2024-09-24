68a7bd6548ef4a5dd3739503f5f41dd8915aa2c7
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-09-24 07:18:41 +0100

    .

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -8,19 +8,20 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ComicShannsMono Nerd Font:monospace:size=14" };
 static const char dmenufont[]       = "ComicShannsMono Nerd Font:monospace:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#504945";
+static const char col_gray3[]       = "#bdae93";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_cyan[]        = "#cc241d";
+static const char col_yellow[]        = "#d79921";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray2,  col_yellow  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "󰖟", "", "", "", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
