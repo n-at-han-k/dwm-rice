f900cbc37286b873d37a0838e568c04388a98962
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-11-08 10:26:43 +0000

    adjust border gap

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 15;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
